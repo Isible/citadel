e3e347119e8925965f0fcb045f9eee1c505bee5c
thepigcat <benpospo@gmail.com>
2023-12-10 00:58:28 +0100

    fix lexer

--- test-lang/src/lexer.rs
+++ test-lang/src/lexer.rs
@@ -1,86 +1,54 @@
-use std::{error::Error, fmt::Display};
-
 use crate::tokens::Token;
 
 pub struct Lexer {
-    stream: Vec<char>,
-
-    cur_char: char,
-    next_char: Option<char>,
-
+    input: String,
+    cur_char: Option<char>,
     cur_pos: usize,
+    next_pos: usize,
 }
 
 impl Lexer {
-    pub fn new(input: String) -> Result<Self, LexerError> {
-        let chars: Vec<char> = input.chars().collect();
-        Ok(Self {
-            cur_char: match chars.get(0) {
-                Some(ch) => *ch,
-                None => return Err(LexerError("Invalid input string. Most likely due to being empty")),
-            },
-            next_char: match chars.get(1) {
-                Some(ch) => Some(*ch),
-                None => None,
-            },
-            cur_pos: 1,
-            stream: chars,
-        })
+    pub fn new(input: String) -> Self {
+        let mut lexer = Self { input, cur_char: None, cur_pos: 0, next_pos: 0 };
+        lexer.next_char();
+        lexer
     }
 
-    fn next_char(&mut self) -> Result<(), LexerError> {
-        self.cur_char = match self.next_char {
-            Some(ch) => ch,
-            None => return Err(LexerError("Failed to get next token. End of file")),
-        };
-        self.cur_pos += 1;
-        self.next_char = match self.stream.get(self.cur_pos) {
-            Some(ch) => Some(*ch),
-            None => None,
-        };
-        return Ok(());
-    }
-
-    pub fn tokenize(&mut self) -> Result<Token, LexerError> {
+    pub fn tokenize(&mut self) -> Token {
         self.skip_whitespace();
-        match self.cur_char {
-            c if c.is_numeric() => {
-                let mut num = String::new();
-                while self.cur_char.is_numeric() {
-                    num.push(self.cur_char);
-                    match self.next_char() {
-                        Ok(_) => (),
-                        Err(_) => return Ok(Token::Integer(num.parse().expect("Failed to parse string"))),
+        let tok = match self.cur_char {
+            Some(ch) => match ch {
+                c if c.is_numeric() => {
+                    let first_pos = self.cur_pos;
+                    while self.cur_char.is_some() && self.cur_char.unwrap().is_numeric() {
+                        self.next_char();
                     }
-                }
-                return Ok(Token::Integer(num.parse().expect("Failed to parse string")))
-            }
-            _ => (),
-        }
-        return Err(LexerError("No token found"));
+                    let string: String = self.input[first_pos..self.cur_pos].into();
+                    Token::Integer(string.parse().unwrap())
+                },
+                _ => todo!()
+            },
+            None => todo!(),
+        };
+        tok
+    }
+
+    fn next_char(&mut self) {
+        self.cur_pos = self.next_pos;
+        self.cur_char = self.input.chars().nth(self.cur_pos);
+        self.next_pos += 1;
     }
 
     fn skip_whitespace(&mut self) {
-        while self.cur_char.is_whitespace() || self.cur_char == '\n' {
-            match self.next_char() {
-                Ok(_) => (),
-                Err(_) => break,
-            }
+        match self.cur_char {
+            Some(mut ch) => while ch.is_whitespace() {
+                self.next_char();
+                match self.cur_char {
+                    Some(cch) => ch = cch,
+                    None => return,
+                }
+            },
+            None => return,
         }
-    }
-
-    fn debug_cur_char(&self) {
-        dbg!("Current char: {}", self.cur_char);
-    }
-}
-
-#[derive(Debug)]
-pub struct LexerError(&'static str);
-
-impl Error for LexerError {}
-
-impl Display for LexerError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_str(self.0)
     }
 }
--- test-lang/src/main.rs
+++ test-lang/src/main.rs
@@ -2,13 +2,14 @@ use lexer::Lexer;
 
 pub mod lexer;
 pub mod tokens;
+mod tests;
 
 fn main() {
     run();
 }
 
 fn run() {
-    let mut lexer = Lexer::new(String::from("1000 10002")).expect("Lexer errored");
-    println!("Token: {:?}", lexer.tokenize().expect("Failed to tokenize"));
-    println!("Token: {:?}", lexer.tokenize().expect("Failed to tokenize"))
+    let mut lexer = Lexer::new(String::from("      11000 10002"));
+    println!("Token: {:?}", lexer.tokenize());
+    println!("Token: {:?}", lexer.tokenize());
 }
