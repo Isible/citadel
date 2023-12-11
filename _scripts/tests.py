64815738f8908ce48fd44330c03ce657c9fae2e1
thepigcat <benpospo@gmail.com>
2023-12-11 15:32:41 +0100

    migrate scripts to python

---
+++ _scripts/tests.py
@@ -0,0 +1,35 @@
+import subprocess
+import sys
+
+test_cmds = {
+    "api": "cargo test --package api --lib -- tests --nocapture",
+    "frontend": "cargo test --package frontend --lib -- tests --nocapture",
+    "middleend": "cargo test --package middleend --lib -- tests --nocapture",
+    "backend": "cargo test --package backend --lib -- tests --nocapture",
+    "fungus": "cargo test --package fungus --bin fungus -- tests --nocapture",
+    "test-lang": "cargo test --package test-lang --bin test-lang -- tests --nocapture",
+}
+
+def runTest(selection: str):
+    match selection:
+        case "*":
+            for i in test_cmds.values():
+                result = subprocess.run(i, shell=True, capture_output=True, text=True)
+                print(result.stdout, end="")
+        case _:
+            result = subprocess.run(test_cmds[selection], shell=True, capture_output=True, text=True)
+            print(result.stdout, end="")
+
+
+print("What tests should be ran?")
+print("""Valid:
+- `fungus` - the rust compiler
+- `test-lang` - language for testing citadel
+- `api` - citadel api for devs
+- `frontend` - the compiler frontend (IR-gen)
+- `middleend` - the compiler middleend (Optimizer)
+- `backend` - the compiler backend (Machine code gen)
+- `*` - run tests for all subprojects""")
+selection = input()
+
+runTest(selection)
--- _scripts/tests.sh
+++
@@ -1,5 +0,0 @@
-cargo test --package api --lib -- tests --nocapture
-cargo test --package frontend --lib -- tests --nocapture
-cargo test --package middleend --lib -- tests --nocapture
-cargo test --package backend --lib -- tests --nocapture
-cargo test --package fungus --bin fungus -- tests --nocapture
